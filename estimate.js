a558810031a7be26ee0f15aa42a96aa51efa34e5
Martin Knoller Stocker <knm@imis.ch>
2011-11-28 21:04:32 +0100

    initial commit

---
+++ estimate.js
@@ -0,0 +1,118 @@
+var totsignature = '&#956;<sub>tot</sub>/&#963;<sub>tot</sub>';
+var signature = ' (&#956;/&#963;)';
+
+isNumber = function(candidate)
+{
+    return !isNaN(parseInt(candidate));
+}
+
+popOutCSV = function()
+{
+    var header = 'description,O,N,P,duration,deviation\n';
+    var data = header;
+    $('.tasks ul li').each(
+            function()
+            {
+                $(this).find('input').each(function(){
+                data += $(this).val() + ',';
+                });
+                var mu = $(this).find('span.mu').html();
+                data += (mu || '') + ',';
+                var sigma = $(this).find('span.sigma').html();
+                data += sigma || '';
+                data += '\n';
+            });
+    window.location='data:text/csv;charset=utf8,' + encodeURIComponent(data);
+}
+
+updateNumbers = function()
+{
+    var mu_tot = 0;
+    var sigma_tot = 0;
+    $('.tasks input[type=number]').each(function(){
+        validate($(this));
+        });
+
+    $('.tasks > ul > li').each(
+           function(){
+               var opt = $(this).children('input.optimus').val();
+               var norm = $(this).children('input.nominal').val();
+               var worst = $(this).children('input.worst').val();
+               if(isNumber(opt) && isNumber(norm) && isNumber(worst))
+                {
+                    var mu = (parseFloat(opt) + 4 * parseFloat(norm) + parseFloat(worst))/6.0;
+                    var sigma = (parseFloat(worst) - parseFloat(opt))/6.0;
+                    $(this).find('.sum').remove();
+                    $('<span class="sum">' + signature + 
+                        ' <span class="mu">' + mu.toFixed(2) + 
+                        '</span>/<span class="sigma">' + sigma.toFixed(2) + 
+                        '</span></span>').insertAfter($(this).find('input.worst'));
+                    mu_tot += parseFloat(mu);
+                    sigma_tot += sigma * sigma;
+                }
+           });
+    $('.summary').empty().append($('<span class="totsum">' + totsignature + '<br />' + mu_tot.toFixed(2) + '/' + Math.sqrt(sigma_tot).toFixed(2) + '</span>'));
+}
+
+validate = function(input)
+{
+    if(isNumber(input.val()))
+    {
+        input.removeClass('error');
+    }
+    else
+    {
+        input.addClass('error');
+    }
+}
+
+init = function()
+{
+    $('input').live('click',function(){
+        $(this).select();
+      });
+
+    $('input[type=number]').live('keyup',function(){
+        updateNumbers();
+      });
+
+    $('img.add').live('click',function(event)
+            {
+                event.stopImmediatePropagation();
+                var task = $('.cloner > li').clone(true);
+                task.insertAfter( $(this).closest('li'));
+            });
+    $('img.delete').live('click',function()
+            {
+                if($('.tasks ul li').size() > 1)
+                {
+                    $(this).closest('li').remove();
+                    updateNumbers();
+                }
+            });
+
+    $('img.clear').click(
+            function(){
+                $('.tasks ul li').slice(1).remove();
+                $('.tasks ul li input.optimus').removeClass('error').val('O');
+                $('.tasks ul li input.nominal').removeClass('error').val('N');
+                $('.tasks ul li input.worst').removeClass('error').val('P');
+                $('.tasks ul li input.description').val('description');
+            });
+
+    $('img.download').click(
+            function(event)
+            {
+                event.stopImmediatePropagation();
+                popOutCSV();
+            });
+
+    $('.catcher').click(
+            function()
+            {
+                $('.explanation').slideToggle('slow');
+            });
+
+    shortcut.add('Alt+Ctrl+B', function(){console.log('pressed key')});
+    $('.tasks ul li input:first').select();
+}// end init
